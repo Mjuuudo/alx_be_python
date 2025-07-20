511044eae3229c7bb945b7b42965afe4d7067842
Mjuuudo <abdelmajid.aitouaziz@gmail.com>
2025-07-20 20:11:26 +0100

    Pattern drawing Task Done

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,7 @@
 size = int (input ("Enter the size of the pattern: "))
-for i in range (0, size) :
+i = 0
+while (i < size) :
     for j in range (0, size) :
         print ("*", end='')
+    i += 1
     print ()
